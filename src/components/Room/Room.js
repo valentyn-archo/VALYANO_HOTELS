e898d0072e6e62662099fec81e21c8b7cd7aae51
Valentyn Vyshyvan <vyshyvanv.official@gmail.com>
2020-04-13 20:01:42 +0300

    add image carousel, change Room component structure, change base and additional styles, change data.js file structure

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,22 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import AwesomeSlider from "react-awesome-slider";
+import AwsSliderStyles from "react-awesome-slider/src/styles";
 import defaultImg from "../../images/room-1.jpeg";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+
+import "./Room.css";
 
 export default function Room({ room }) {
   const { name, slug, images, price } = room;
   return (
     <article className="room">
-      <div className="img-container">
-        <img src={images[0] || defaultImg} alt="room option" />
-        <div className="price-top">
-          <h6>${price}</h6>
-          <p>per night</p>
+      {images.length === 1 ? (
+        <div className="single-img-container">
+          <img src={images.url || defaultImg} alt="room option" />
+          <div className="price-top">
+            <h6>${price}</h6>
+            <p>per night</p>
+          </div>
+          <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+            Features
+          </Link>
         </div>
-        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
-          Features
-        </Link>
-      </div>
+      ) : (
+        <div className="img-container">
+          <AwesomeSlider cssModule={AwsSliderStyles} bullets={false}>
+            {images.map((image) => {
+              return (
+                <div key={image.id} data-src={image.url}>
+                  <div className="price-top">
+                    <h6>${price}</h6>
+                    <p>per night</p>
+                  </div>
+                </div>
+              );
+            })}
+          </AwesomeSlider>
+        </div>
+      )}
       <p className="room-info">{name}</p>
     </article>
   );
