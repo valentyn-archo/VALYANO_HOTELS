8d7d19b39f7d13a00aed38902dc19c0e86fa3817
Valentyn Vyshyvan <vyshyvanv.official@gmail.com>
2020-04-14 17:55:52 +0300

    fix code after rebase

--- src/context/context.js
+++ src/context/context.js
@@ -42,7 +42,7 @@ class RoomProvider extends Component {
   formatData(items) {
     let tempItems = items.map((item) => {
       let id = item.sys.id;
-      let images = item.fields.images.map((images) => images.fields.file.url);
+      let images = item.fields.images;
       let coordinates = item.coordinates;
       let room = { ...item.fields, images, id, coordinates };
 
@@ -96,86 +96,9 @@ class RoomProvider extends Component {
       tempRooms = tempRooms.filter((room) => room.type === type);
     }
 
-    formatData(items) {
-        let tempItems = items.map(item => {
-            let id = item.sys.id;
-            let images = item.fields.images.map(images => images);
-            let coordinates = item.coordinates;
-            let room = {...item.fields, images, id, coordinates}
-
-            return room;
-        });
-
-        return tempItems;
-    };
-
-    getRoom = (slug) => {
-        let tempRooms = [...this.state.rooms];
-        const room = tempRooms.find(room => room.slug === slug);
-
-        return room;
-    };
-
-    handlechange = event => {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = event.target.name;
-
-        this.setState({
-            [name]: value
-        }, this.filterRooms)
-    };
-
-    filterRooms = () => {
-        let {
-            rooms,
-            type,
-            capacity,
-            price,
-            minSize,
-            maxSize,
-            breakfast,
-            pets
-        } = this.state;
-
-        // all the rooms
-        let tempRooms = [...rooms];
-
-        // transform value
-        capacity = parseInt(capacity);
-        price = parseInt(price);
-
-
-        // filter by type
-        if (type !== 'all') {
-            tempRooms = tempRooms.filter(room => room.type === type)
-        };
-
-        // filter by capacity
-        if (capacity !== 1) {
-            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
-        };
-
-        // filter by price
-        tempRooms = tempRooms.filter(room => room.price <= price);
-
-        // filter by size
-        tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize);
-
-        // filter by breakfast
-        if (breakfast) {
-            tempRooms = tempRooms.filter(room => room.breakfast === true)
-        }
-
-        // filter by pets
-        if (pets) {
-            tempRooms = tempRooms.filter(room => room.pets === true)
-        }
-
-        // change state
-        this.setState({
-            sortedRooms: tempRooms
-        })
+    // filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
     }
 
     // filter by price
