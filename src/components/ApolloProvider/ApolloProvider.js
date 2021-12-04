727b8f7f14b499045c9d9b3e7e3edd7d2c4e9ccd
vvyshyvan <vyshyvanv.official@gmail.com>
2021-12-04 15:58:16 +0200

    Fix AWS Deployment Issues

--- src/components/ApolloProvider/ApolloProvider.js
+++ src/components/ApolloProvider/ApolloProvider.js
@@ -2,40 +2,11 @@ import React from 'react';
 import {
     ApolloClient,
     InMemoryCache,
-    ApolloProvider as Provider,
-    createHttpLink,
-    split
+    ApolloProvider as Provider
 } from '@apollo/client';
-import {WebSocketLink} from '@apollo/client/link/ws';
-import {getMainDefinition} from '@apollo/client/utilities';
-
-let httpLink = createHttpLink({
-    uri: '/graphql/'
-});
-
-const host = window.location.host;
-
-const wsLink = new WebSocketLink({
-    uri: `ws://${host}/graphql/`,
-    options: {
-        reconnect: true
-    }
-});
-
-const splitLink = split(
-    ({query}) => {
-        const definition = getMainDefinition(query);
-        return (
-            definition.kind === 'OperationDefinition' &&
-            definition.operation === 'subscription'
-        );
-    },
-    wsLink,
-    httpLink
-);
 
 const client = new ApolloClient({
-    link: splitLink, // uri: 'http://localhost:4000/graphql/'
+    uri: 'http://localhost:4000/graphql/',
     cache: new InMemoryCache()
 });
 
